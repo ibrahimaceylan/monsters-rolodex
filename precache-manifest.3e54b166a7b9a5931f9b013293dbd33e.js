2324a45d5aa38f5913bc25c5909cf0a98cace9e2
Ibrahim Ceylan <ibrahim.ceylan@ceinteraktif.co.uk>
2019-11-13 17:00:48 +0000

    Updates

---
+++ precache-manifest.3e54b166a7b9a5931f9b013293dbd33e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b17cdbe67bf43a70c4541600d598da4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "be48c088ed17abb72bbb",
+    "url": "/monsters-rolodex/static/css/main.90fb04fa.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/monsters-rolodex/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "be48c088ed17abb72bbb",
+    "url": "/monsters-rolodex/static/js/main.55995cd6.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
